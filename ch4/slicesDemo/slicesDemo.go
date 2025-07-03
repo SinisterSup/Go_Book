5824b9e8c7ec3411a3414605c8c3c4d41dbc67f7
SinisterSup <mheist456@gmail.com>
2025-07-03 21:48:39 +0530

    add Exercise challenges

--- ch4/slicesDemo/slicesDemo.go
+++ ch4/slicesDemo/slicesDemo.go
@@ -190,3 +190,15 @@ func main() {
 	slice := []int{5, 6, 7, 8, 9}
 	fmt.Println(remove(slice, 2)) // Remove element at index 2, output: [5 6 8 9]
 }
+
+/* Exercise 4.3: Rewrite reverse to use an array pointer instead of a slice. */
+
+/* Exercise 4.4: Write a version of rotate that operates in a single pass. */
+
+/* Exercise 4.5: Write an in-place function to eliminate adjacent duplicates in a []string slice. */
+
+/* Exercise 4.6: Write an in-place function that squashes each run of adjacent Unicode spaces
+* (see unicode.IsSpace) in a UTF-8 encoded []byte slice into a single ASCII space. */
+
+/* Exercise 4.7: Modify reverse to reverse the characters of a []byte slice that represents a
+* UTF-8 encoded string, in place. Can you do it without allocating new memory? */
